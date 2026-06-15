d0db8c093bd5c0cf096a257e53f9a0815dd2f1f5
patrickromanug-boop <patrickromanug@gmail.com>
2026-06-15 13:32:22 +0300

    Update route.ts

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,1 +1,13 @@
 
+import { createClient } from '@/lib/supabase/server'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+  const { searchParams, origin } = new URL(req.url)
+  const code = searchParams.get('code')
+  if (code) {
+    const supabase = await createClient()
+    await supabase.auth.exchangeCodeForSession(code)
+  }
+  return NextResponse.redirect(`${origin}/admin/dashboard`)
+}
