993f424b5fdb277eced1cd2619854699276af8ca
patrickromanug-boop <patrickromanug@gmail.com>
2026-06-16 20:51:08 +0300

    Initialize Resend with API key and email constants

--- lib/resend.ts
+++ lib/resend.ts
@@ -1,1 +1,6 @@
 
+import { Resend } from 'resend'
+
+export const resend = new Resend(process.env.RESEND_API_KEY)
+export const SCHOOL_EMAIL = process.env.ADMIN_EMAIL!
+export const FROM_EMAIL = 'Vision African Child <no-reply@visionafricanchild.ug>'
