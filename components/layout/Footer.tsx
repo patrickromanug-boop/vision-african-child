4a32fe6ef252b76b0a9a0107bafc997f53dd08ed
patrickromanug-boop <patrickromanug@gmail.com>
2026-06-17 14:28:51 +0300

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,12 +1,11 @@
 <div className="flex gap-2">
   {[Facebook, Twitter, Instagram, Youtube].map((Icon, i) => (
-    
+    <button
       key={i}
-      href="#"
+      onClick={() => alert('Social media pages coming soon!')}
       className="w-9 h-9 rounded-lg bg-white/10 hover:bg-[#F4720B] flex items-center justify-center transition-colors"
-      onClick={(e) => { e.preventDefault(); alert('Social media pages coming soon!') }}
     >
       <Icon size={15} />
-    </a>
+    </button>
   ))}
 </div>
